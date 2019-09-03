090d85bca0a44ab2b227b5a07ee67b17400917ec
gauravapandey <gaurav.pandey@tothenew.com>
2019-09-03 17:21:17 +0530

    commit no. 2

--- js/customJS.js
+++ js/customJS.js
@@ -1,4 +1,22 @@
+// js used in header
+
 function changeHamburger(x) {
     x.classList.toggle("change");
     $('.menu-list-sm').toggleClass("animated-dropdown");
-  }
+}
+
+// end of js used in header
+
+// js used in page-1
+
+function changeTab(id){
+    $('.triangle').addClass("display-none");
+    if(id==="Bologsbestallningar"){
+        $('#Bologsbestallningar').removeClass("display-none");
+    }
+    if(id==="Aktiebocker"){
+        $("#Aktiebocker").removeClass("display-none");
+    }
+}
+
+// end of js used in page-1
