90fe1c58f0bd7d456371fde285bf88a9fcd37c0c
gauravapandey <gaurav.pandey@tothenew.com>
2019-09-09 10:23:40 +0530

    page 3 is add and yet to be complete

--- js/customJS.js
+++ js/customJS.js
@@ -10,12 +10,25 @@ function changeHamburger(x) {
 // js used in page-1
 
 function changeTab(id){
+    $('.aktiebocker-border-bottom').css("border-bottom","0px");
+    $('.bologsbestallningar-border-bottom').css("border-bottom","0px");
     $('.triangle').addClass("display-none");
     if(id==="Bologsbestallningar"){
         $('#Bologsbestallningar').removeClass("display-none");
+        $('.bologsbestallningar-border-bottom').css("border-bottom","5px solid #fb6a3f");
     }
     if(id==="Aktiebocker"){
         $("#Aktiebocker").removeClass("display-none");
+        $('.aktiebocker-border-bottom').css("border-bottom","5px solid #fb6a3f");
+    }
+}
+
+console.log(screen.width);
+if(screen.width < 768){
+    console.log(screen.width);
+    function expandSearchBar(){
+        $('btn-div').addClass("display-none");
+        $('search-div').css("flex-basis","100%");
     }
 }
 
